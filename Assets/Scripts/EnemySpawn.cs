90f6bdf28a9f7661dec31d40fd06c78ed0e1ab81
incarik <incarik1@gmail.com>
2024-04-03 10:28:02 +0200

    terminado por hoy

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -36,10 +36,11 @@ public class EnemySpawn : MonoBehaviour
             //Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
 
             //Esto es para el array
-            Instantiate(enemyPrefab, spawnPoint[0].position, spawnPoint[0].rotation);
-            Instantiate(enemyPrefab, spawnPoint[1].position, spawnPoint[1].rotation);
-            Instantiate(enemyPrefab, spawnPoint[2].position, spawnPoint[2].rotation);
+            /*Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Length)], spawnPoint[0].position, spawnPoint[0].rotation);
+            Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Length)], spawnPoint[1].position, spawnPoint[1].rotation);
+            Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Length)], spawnPoint[2].position, spawnPoint[2].rotation);*/
 
+            Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Length)], spawnPoint[Random.Range(0, enemyPrefab.Length)].position, spawnPoint[Random.Range(0, enemyPrefab.Length)].rotation);
             timer = 0;
         }
         
